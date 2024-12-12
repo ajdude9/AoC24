3f9949df531e55ec654ed9671ea5d85f16d5614b
ajdude9 <adamtheteeth@hotmail.com>
2024-12-12 15:02:20 +0000

    Added solution for Day 3-2

--- Assets/Scripts/DayThreeScript.cs
+++ Assets/Scripts/DayThreeScript.cs
@@ -12,10 +12,9 @@ public class DayThreeScript : MonoBehaviour
     string input;
     public void run1()
     {
+        
         readFiles();
         string pattern = @"mul\([0-9]{1,3}\,[0-9]{1,3}\)";
-        string enabler = @"do()";
-        string disabler = @"don't()";
         string numbers = @"[0-9]{1,3}\,[0-9]{1,3}";
         int counter = 0;
         int finalInt = 0;
@@ -38,10 +37,57 @@ public class DayThreeScript : MonoBehaviour
             finalInt = finalInt + multiplyMap[i];
         }
         Debug.Log(finalInt);
+        
     }
 
     public void run2()
     {
+        readFiles();
+        string pattern = @"(mul\([0-9]{1,3}\,[0-9]{1,3}\))|(do\(\))|(don't\(\))";
+        string numbers = @"[0-9]{1,3}\,[0-9]{1,3}";
+        MatchCollection matches = Regex.Matches(input, pattern);
+        int counter = 0;
+        int finalInt = 0;
+        bool allowMul = true;
+        List<string> literals = new List<string>();
+        List<int> multiplyMap = new List<int>();
+        foreach (Match match in matches)
+        {
+            string literal = match.ToString();
+            literals.Add(literal);//Add every found RegEx item into an ordered list
+
+        }
+        for (int i = 0; i < literals.Count; i++)//Parse through the list of every matched RegEx find - either mul, do or don't
+        {
+            string identifier = literals[i].Substring(0, 4);//Read the first four characters of the string, resulting in either 'mul(', 'do()' or "don'")
+            switch (identifier)//Act based on what is found
+            {
+                case "mul("://If it's a multiply statement
+                    if (allowMul)//If multiplication is currently allowed
+                    {
+                        MatchCollection numberMatches = Regex.Matches(literals[i], numbers);
+                        foreach (Match numMatch in numberMatches)
+                        {
+                            string literalNumbers = numMatch.ToString();
+                            string[] nums = literalNumbers.Split(char.Parse(","));
+                            multiplyMap.Add(int.Parse(nums[0]) * int.Parse(nums[1]));
+                        }
+                    }
+                    break;
+                case "do()"://If it's a do statement
+                    allowMul = true;//Enable multiplication
+                    break;
+                case "don'"://If it's a don't statement
+                    allowMul = false;//Disable multiplication
+                    break;
+
+            }
+        }
+        for(int i = 0; i < multiplyMap.Count; i++)
+        {
+            finalInt = finalInt + multiplyMap[i];
+        }
+        Debug.Log(finalInt);
 
     }
 
